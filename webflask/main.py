5e32f655de7041470988a01255b669ba46000323
lilian920507 <100552462+lilian920507@users.noreply.github.com>
2022-04-30 14:42:05 +0800

    aa

--- webflask/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route("/")
-def running():
-    return render_template("index.html")
-
-if __name__=="__main__":
-    app.run(debug=True)
-# you should end all the running of the terminal then you could run it.
